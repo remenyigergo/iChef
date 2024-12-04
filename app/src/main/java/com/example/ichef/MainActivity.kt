190c231b30e83afd2e1a26b495578b8ef1f65a0d
gremenyi <greg.remenyi@gmail.com>
2024-12-04 21:36:30 +0100

    First commit setting the basic visual for the basic navigation fragments.Top bar added
    Bottom Navbar added
    Icons added

---
+++ app/src/main/java/com/example/ichef/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.ichef
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge() to have a full top bar to edge. in future this can be enabled and top bar needs deletion.
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+
+}
