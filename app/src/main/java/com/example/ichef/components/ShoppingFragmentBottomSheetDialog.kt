0276db9760be1037f6ad88c1c3dfedbc52f69aae
gremenyi <greg.remenyi@gmail.com>
2024-12-23 16:48:42 +0100

    WIP Experimenting with the bottom sheet dialog. Already working with 1 theme. Config change breaks.

---
+++ app/src/main/java/com/example/ichef/components/ShoppingFragmentBottomSheetDialog.kt
@@ -0,0 +1,5 @@
+package com.example.ichef.components
+
+interface ShoppingFragmentBottomSheetDialog {
+    fun show()
+}
--- app/src/main/java/com/example/ichef/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/ichef/fragments/HomeFragment.kt
@@ -5,18 +5,38 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.ichef.R
+import com.example.ichef.components.ShoppingFragmentBottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+
+    @Inject
+    lateinit var bottomSheetDialogController: ShoppingFragmentBottomSheetDialog
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         Log.d("HomeFragment", "In onCreateView")
-        return inflater.inflate(R.layout.home_fragment, container, false)
+
+        val rootView = inflater.inflate(R.layout.home_fragment, container, false)
+
+        return rootView
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Handle dialog show when the fragment is resumed after configuration change (like theme change)
+        if (isAdded) {
+            bottomSheetDialogController.show()
+        }
+    }
+
 }
